6525f6b581ad45cf883db9f6713540aa2511304c
Kenichi Ishigaki <ishigaki@cpan.org>
2011-10-18 01:20:13 +0900

    releng

--- lib/Acme/CPANAuthors.pm
+++ lib/Acme/CPANAuthors.pm
@@ -5,7 +5,7 @@ use warnings;
 use Carp;
 use Acme::CPANAuthors::Utils qw( cpan_authors cpan_packages );
 
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 
 sub new {
   my ($class, @categories) = @_;
