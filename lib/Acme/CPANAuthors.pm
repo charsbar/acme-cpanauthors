3b60755beb6b360e680b3234c22f3032dba7167b
Kenichi Ishigaki <ishigaki@cpan.org>
2012-03-10 02:03:44 +0900

    updated copyright year

--- lib/Acme/CPANAuthors.pm
+++ lib/Acme/CPANAuthors.pm
@@ -393,7 +393,7 @@ Kenichi Ishigaki, E<lt>ishigaki at cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2007-2011 by Kenichi Ishigaki.
+Copyright (C) 2007-2012 by Kenichi Ishigaki.
 
 This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
